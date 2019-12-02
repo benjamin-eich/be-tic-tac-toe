8afda4a296f3b59c88bcf325050683478b21b5d9
Benjamin Eich <benjamin-eich@web.de>
2019-12-02 18:41:29 +0100

    WordPress PHP Coding Standards Docs compliance

--- class-betictactoe-gamestate.php
+++ class-betictactoe-gamestate.php
@@ -2,11 +2,15 @@
 /**
  * BE Tic Tac Toe
  *
+ * A little plugin that provides a shortcode to play Tic Tac Toe.
+ *
  * @package           BETicTacToe
  * @author            Benjamin Eich
  * @copyright         2019 Your Name or Company Name
  * @license           GPL-2.0-or-later
- *
+ */
+
+/**
  * BE Tic Tac Toe is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -24,19 +28,57 @@
 /**
  * BETicTacToe_GameState
  *
- * This stores the game's state and handles serialization
+ * This stores the game's state and handles serialization.
+ * This is exchanged via ajax with the frontend.
  */
 class BETicTacToe_GameState implements JsonSerializable {
+	/**
+	 * Contains the current state of the 9 squares.
+	 *
+	 * @var array $squares
+	 */
 	public $squares;
+
+	/**
+	 * Contains the winner if there is one.
+	 *
+	 * @var string $winner
+	 */
 	public $winner;
+
+	/**
+	 * Boolean showing who's turn it is.
+	 *
+	 * @var boolean $x_is_next
+	 */
 	public $x_is_next;
+
+	/**
+	 * Containes the field index of the next move AI would make.
+	 *
+	 * @var integer $best_next_move
+	 */
 	public $best_next_move;
+
+	/**
+	 * Contains the version for the gamestate schema.
+	 *
+	 * Upon initialization the gamestate will reset if an older version is stored in the session.
+	 *
+	 * @var integer $version
+	 */
 	public static $version = 2;
 
+	/**
+	 * Constructor resets the gamestate.
+	 */
 	public function __construct() {
 		$this->reset();
 	}
 
+	/**
+	 * Resets the gamestate to an empty gamestate.
+	 */
 	public function reset() {
 		$this->squares        = array(
 			null,
@@ -54,6 +96,9 @@ class BETicTacToe_GameState implements JsonSerializable {
 		$this->best_next_move = null;
 	}
 
+	/**
+	 * Implementation of the serialization for JsonSerializable interface
+	 */
 	public function jsonSerialize() {
 		return array(
 			'squares'        => $this->squares,
--- class-betictactoe.php
+++ class-betictactoe.php
@@ -2,11 +2,15 @@
 /**
  * BE Tic Tac Toe
  *
+ * A little plugin that provides a shortcode to play Tic Tac Toe.
+ *
  * @package           BETicTacToe
  * @author            Benjamin Eich
  * @copyright         2019 Your Name or Company Name
  * @license           GPL-2.0-or-later
- *
+ */
+
+/**
  * BE Tic Tac Toe is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -21,6 +25,9 @@
  * along with Tic Tac Toe. If not, see {URI to Plugin License}.
  */
 
+/**
+ * Requires the gamestate's class
+ */
 require_once 'class-betictactoe-gamestate.php';
 
 /**
@@ -59,18 +66,27 @@ class BETicTacToe {
 		}
 	}
 
+	/**
+	 * Starts a new Session.
+	 */
 	private static function register_session_new() {
 		if ( ! session_id() ) {
 			session_start();
 		}
 	}
 
+	/**
+	 * Initializes the game's shortcode
+	 */
 	private static function init_shortcodes() {
 		add_shortcode( TICTACTOE__SHORTCODE_GAME, array( self::class, 'shortcode_game' ) );
 	}
 
 	/**
-	 * Shortcodes
+	 * Shortcode implementation
+	 *
+	 * @param array  $atts    Attributes of the shortcode.
+	 * @param string $content Content enclosed by the shortcode.
 	 */
 	public static function shortcode_game( $atts = array(), $content = null ) {
 		$state = self::game_get_state();
@@ -93,10 +109,13 @@ class BETicTacToe {
 		return $content;
 	}
 
+	/**
+	 * Enqeues JavaScript and CSS.
+	 */
 	public static function enqueue_frontend_stuff() {
 		wp_enqueue_script( 'tic-tac-toe-javascript', plugins_url( '/js/tic-tac-toe.js', __FILE__ ), array( 'jquery' ), '1.0.0', true );
 
-		// in JavaScript, object properties are accessed as ajax_object.ajax_url, ajax_object.we_value
+		// in JavaScript, object properties are accessed as ajax_object.ajax_url, ajax_object.we_value.
 		$ajax_nonce = wp_create_nonce( 'tic-tac-toe-ajax-nonce' );
 		wp_localize_script(
 			'tic-tac-toe-javascript',
@@ -110,6 +129,9 @@ class BETicTacToe {
 		wp_enqueue_style( 'tic-tac-toe-styles', plugins_url( '/css/tic-tac-toe.css', __FILE__ ), array(), '1.0.0' );
 	}
 
+	/**
+	 * Ajax function to receive the current gamestate.
+	 */
 	public static function game_ajax_get_state() {
 		check_ajax_referer( 'tic-tac-toe-ajax-nonce', 'security' );
 
@@ -117,6 +139,9 @@ class BETicTacToe {
 		wp_die();
 	}
 
+	/**
+	 * Ajax function to reset the gamestate.
+	 */
 	public static function game_ajax_reset_state() {
 		check_ajax_referer( 'tic-tac-toe-ajax-nonce', 'security' );
 
@@ -126,15 +151,19 @@ class BETicTacToe {
 		wp_die();
 	}
 
+
+	/**
+	 * Ajax function to set a player's move.
+	 */
 	public static function game_ajax_set_move() {
 		check_ajax_referer( 'tic-tac-toe-ajax-nonce', 'security' );
 
-		$square = $_POST['square'];
-		$player = $_POST['player'];
+		$square = isset( $_POST['square'] ) ? sanitize_text_field( wp_unslash( $_POST['square'] ) ) : null;
+		$player = isset( $_POST['player'] ) ? sanitize_text_field( wp_unslash( $_POST['player'] ) ) : null;
 
 		self::make_move( $square, $player );
 
-		// Do AI move
+		// Do AI move.
 		$state = self::game_get_state();
 		if ( true === $state->x_is_next ) {
 			self::make_move( $state->best_next_move, TICTACTOE__SYMBOL_PLAYER_X );
@@ -144,8 +173,13 @@ class BETicTacToe {
 		wp_die();
 	}
 
+	/**
+	 * Applies a move to the gamestate.
+	 *
+	 * @param array  $square The current board.
+	 * @param string $player The current player.
+	 */
 	private static function make_move( $square, $player ) {
-
 		$state = self::game_get_state();
 
 		if ( null === $state->squares[ $square ] ) {
@@ -160,10 +194,16 @@ class BETicTacToe {
 		$_SESSION[ TICTACTOE__SESSION_STATE_VAR_NAME ] = $state;
 	}
 
+	/**
+	 * Resets the gamestate.
+	 */
 	private static function game_reset_state() {
 		$_SESSION[ TICTACTOE__SESSION_STATE_VAR_NAME ] = new BETicTacToe_GameState();
 	}
 
+	/**
+	 * Gets the gamestate.
+	 */
 	private static function game_get_state() {
 		if (
 			! isset( $_SESSION[ TICTACTOE__SESSION_STATE_VAR_NAME ] )
@@ -176,6 +216,11 @@ class BETicTacToe {
 		return $_SESSION[ TICTACTOE__SESSION_STATE_VAR_NAME ];
 	}
 
+	/**
+	 * Calculates the winner.
+	 *
+	 * @param array $squares The current board.
+	 */
 	private static function game_get_winner( $squares ) {
 		$lines = array(
 			array( 0, 1, 2 ),
@@ -196,15 +241,28 @@ class BETicTacToe {
 		return null;
 	}
 
+	/**
+	 * Raw values for minimax algorithm to calculate the best next move.
+	 *
+	 * @var array $player_raw_values Contains the values for the players.
+	 */
 	private static $player_raw_values = array(
 		TICTACTOE__SYMBOL_PLAYER_X => -1,
 		TICTACTOE__SYMBOL_PLAYER_O => 1,
 	);
 
+	/**
+	 * Implementation of recursive minimax algorithm for tic tac toe.
+	 *
+	 * @param array   $board          The board on which to calculate the gametree.
+	 * @param string  $player         The currently active player.
+	 * @param integer $depth          The current depth of the recursion.
+	 * @param integer $best_next_move Output variable containing the best next move (only returned on depth 0).
+	 */
 	private static function minimax( $board, $player, $depth = 0, &$best_next_move = -1 ) {
 		$winner = self::game_get_winner( $board );
 		if ( null !== $winner ) {
-			return self::$player_raw_values[ $winner ] * self::$player_raw_values[ $player ]; // -1 * -1 || 1 * 1
+			return self::$player_raw_values[ $winner ] * self::$player_raw_values[ $player ]; // Either (-1 * -1) or (1 * 1).
 		}
 
 		$move  = -1;
@@ -214,7 +272,7 @@ class BETicTacToe {
 			if ( null === $board[ $i ] ) { // Only possible moves.
 				$board_with_new_move       = $board; // Copy board to make it mutable.
 				$board_with_new_move[ $i ] = $player; // Try the move.
-				$score_for_the_move        = -self::minimax( $board_with_new_move, ( TICTACTOE__SYMBOL_PLAYER_X === $player ? TICTACTOE__SYMBOL_PLAYER_O : TICTACTOE__SYMBOL_PLAYER_X ), $depth + 1 ); // Count negative score for oponnent
+				$score_for_the_move        = -self::minimax( $board_with_new_move, ( TICTACTOE__SYMBOL_PLAYER_X === $player ? TICTACTOE__SYMBOL_PLAYER_O : TICTACTOE__SYMBOL_PLAYER_X ), $depth + 1 ); // Count negative score for oponnent.
 				if ( $score_for_the_move > $score ) {
 					$score = $score_for_the_move;
 					$move  = $i;
